c9cf81fb25fdb229304a18d050a57b3e17f571e4
Justin Irizarry <justinirizarry@Justins-MacBook-Pro.local>
2024-02-20 20:21:11 +1100

    dashboard test

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,25 +7,27 @@ export default function Dashboard() {
   const { steps, userInput } = useSteps();
 
   return (
-    <div>
-      <h1>Dashboard Test</h1>
+    <div className="m-8">
+      <h1 className=" text-5xl">Dashboard Test</h1>
 
-      <div>
-        <h2>Latest User Input:</h2>
-        <p>{userInput || "No user input available"}</p>{" "}
+      <div className="m-8">
+        <h2 className="text-2xl">User Input:</h2>
+        <p>{userInput || "No user input available"}</p>
       </div>
-
-      <h2>Steps</h2>
-      {steps.length > 0 ? (
-        steps.map((step, index) => (
-          <div key={step.id || index}>
+      <div className="m-8">
+        <h2 className="text-2xl ">Steps</h2>
+        {steps.length > 0 ? (
+          <ul className="list-decimal pl-5">
             {" "}
-            <p>{step.content}</p>
-          </div>
-        ))
-      ) : (
-        <p>No steps available.</p>
-      )}
+            {/* Ensure proper padding for the bullet/number */}
+            {steps.map((step, index) => (
+              <li key={step.id || index}>{step.content}</li>
+            ))}
+          </ul>
+        ) : (
+          <p>No steps available.</p>
+        )}
+      </div>
     </div>
   );
 }
